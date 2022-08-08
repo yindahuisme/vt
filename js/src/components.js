6c98237b4dc64431c326a44803b97ef09a6c13dd
yindahu <18579123475@163.com>
2022-08-09 02:22:57 +0800

    update

--- js/src/components.js
+++ js/src/components.js
@@ -1,114 +1,68 @@
-
-
 //素材文件组件
-const metfile_component = Vue.extend({
-    template: document.getElementById('vt-main-metfile-template').innerHTML,
+const matFile_component = Vue.extend({
+    template: document.getElementById('vt_main_matFile_template').innerHTML,
     mounted() {
-    //注册组件实例对象到store
-    this.$store.state.metfile_component = this
+        //初始化
+        //更新素材文件表格数据
+        // this.vt_main_matFile_bodyFullData = this.$store.state.axios_exec()
 
     },
     data() {
         return {
-            // vt_main_metfile_list_header_menuOptions: [{
+            // vt_main_matFile_headerMenuOptions: [{
             //     value: '最后更新时间倒序',
             //     label: '最后更新时间倒序'
             // }, {
             //     value: '创建时间倒序',
             //     label: '创建时间倒序'
             // }],
-            //素材文件列表-sql筛选项编辑框是否打开
-            vt_main_metfile_list_header_edit_dialogVisible: false,
-            //素材文件列表-sql筛选项
-            vt_main_metfile_list_header_menuOptions: [],
-            //素材文件列表-sql筛选项默认选项
-            vt_main_metfile_list_header_menuValue: '',
-            //素材文件列表-素材文件完整信息
-            vt_main_metfile_list_body_full_data: [],
-            //素材文件列表-素材文件表格数据
-            vt_main_metfile_list_body_table_data: [],
-            //素材文件列表-素材文件表格默认选项
-            vt_main_metfile_list_body_table_currentRow: null,
-            //素材文件列表-筛选对话框sql内容
-            vt_main_metfile_list_header_edit_dialog_sqlText: '',
-            // vt_main_metfile_list_body_table_data: [{
-            //     type: '视频',
-            //     key: 'test1.mp4'
-            // }, {
-            //     type: '音频',
-            //     key: 'test2.mp3'
-            // }, {
-            //     type: '图片',
-            //     key: 'test3.jpg'
-            // }, {
-            //     type: '音频',
-            //     key: 'test4.mp3'
-            // }, {
-            //     type: '图片',
-            //     key: 'test5.jpg'
-            // }],
-            // vt_main_metfile_list_body_table_currentRow: {
-            //     type: '音频',
-            //     key: ''
-            // },
-            vt_main_metfile_preview_audio_wavesurfer: null
+            //sql筛选项编辑框是否打开
+            vt_main_matFile_headerEditDialogVisible: false,
+            //sql筛选项
+            vt_main_matFile_headerMenuOptions: [],
+            //sql筛选项默认选项
+            vt_main_matFile_headerMenuValue: '',
+            //完整信息
+            vt_main_matFile_bodyFullData: [],
+            //表格数据
+            vt_main_matFile_bodyTableData: [{
+                type: '视频',
+                key: 'test1.mp4'
+            }, {
+                type: '音频',
+                key: 'test2.mp3'
+            }, {
+                type: '图片',
+                key: 'test3.jpg'
+            }, {
+                type: '音频',
+                key: 'test4.mp3'
+            }, {
+                type: '图片',
+                key: 'test5.jpg'
+            }],
+            //筛选对话框sql内容
+            vt_main_matFile_headerEditDialogSqlText: ''
         }
     },
     methods: {
-        //初始化
-        init: function () {
-            //更新素材文件表格数据
-            // this.vt_main_metfile_list_body_full_data = this.$store.state.axios_exec()
 
-
-
-            //this.$nextTick(() => {
-
-            //        document.getElementById('vt_main_metfile_preview_audio').innerHTML = ''
-            //        //音频波形可视化实例
-            //        this.wavesurfer = WaveSurfer.create({
-            //            container: '#vt_main_metfile_preview_audio',
-            //            waveColor: '#00FA9A',
-            //            progressColor: '#00BFBF',
-            //            backgroundColor: '#e9fff6',
-            //            barWidth: '1'
-            //        })
-            //        this.vt_main_metfile_preview_audio_wavesurfer.load('/met_file/a.mp3')
-
-
-            //})
-
-            //window.onresize=
-            //    function(){
-            //        console.log('resize')
-            //        document.getElementById('vt_main_metfile_preview_audio').innerHTML = ''
-            //        //音频波形可视化实例
-            //        this.wavesurfer = WaveSurfer.create({
-            //            container: '#vt_main_metfile_preview_audio',
-            //            waveColor: '#00FA9A',
-            //            progressColor: '#00BFBF',
-            //            backgroundColor: '#e9fff6',
-            //            barWidth: '1'
-            //        })
-            //        this.vt_main_metfile_preview_audio_wavesurfer.load('/met_file/a.mp3')
-            //    }
-        },
         //获取素材文件列表数据
-        get_vt_main_metfile_list_body_table_data(data_path){
+        get_vt_main_matFile_bodyTableData(data_path) {
             // this.$store.state.axios_exec(
-            //     '/vt/getMetFileInfo', {}, (res) => {
+            //     '/vt/getSettingProperties', {}, (res) => {
             //         this.vt_main_setting_properties = res.data.data
             //         //更新项目数据目录
-            //         this.vt_main_setting_card_projectDataPath = this.vt_main_setting_properties.projectDataPath
+            //         this.vt_main_setting_projectDataPath = this.vt_main_setting_properties.projectDataPath
             //     }
             // )
         },
         //当选中素材文件列表的某一项时触发
-        vt_main_metfile_list_body_table_handleCurrentChange(val) {
-            this.vt_main_metfile_list_body_table_currentRow = val;
+        vt_main_matFile_bodyTableHandleCurrentChange(val) {
+            this.$store.commit('vt_main_matFile_bodyTableCurrentRow',val);
         },
         //动态确定素材列表类型
-        vt_main_metfile_list_body_table_rowClassName({
+        vt_main_matFile_bodyTableRowClassName({
             row
         }) {
             if (row.type == '视频') {
@@ -127,20 +81,68 @@ const metfile_component = Vue.extend({
 })
 
 
-//轨道组件
-const track_component = Vue.extend({
-    template: `
-        <h1> track </h1>
-        `,
+//预览组件
+const pre_component = Vue.extend({
+    template: document.getElementById('vt_main_pre_template').innerHTML,
     mounted() {
-        //注册组件实例对象到store
-        this.$store.state.track_component = this
+        //初始化
+        //this.$nextTick(() => {
+
+        //        document.getElementById('vt_main_pre_audio').innerHTML = ''
+        //        //音频波形可视化实例
+        //        this.wavesurfer = WaveSurfer.create({
+        //            container: '#vt_main_pre_audio',
+        //            waveColor: '#00FA9A',
+        //            progressColor: '#00BFBF',
+        //            backgroundColor: '#e9fff6',
+        //            barWidth: '1'
+        //        })
+        //        this.vt_main_pre_audioWavesurfer.load('/met_file/a.mp3')
+
+
+        //})
+
+        //window.onresize=
+        //    function(){
+        //        console.log('resize')
+        //        document.getElementById('vt_main_pre_audio').innerHTML = ''
+        //        //音频波形可视化实例
+        //        this.wavesurfer = WaveSurfer.create({
+        //            container: '#vt_main_pre_audio',
+        //            waveColor: '#00FA9A',
+        //            progressColor: '#00BFBF',
+        //            backgroundColor: '#e9fff6',
+        //            barWidth: '1'
+        //        })
+        //        this.vt_main_pre_audioWavesurfer.load('/met_file/a.mp3')
+        //    }
+
     },
     methods: {
-        //初始化
-        init: function () {
 
+    },
+    computed: {
+        ...Vuex.mapState([
+            // 素材文件-表格默认选项
+            'vt_main_matFile_bodyTableCurrentRow'
+        ])
+    },
+    data() {
+        return {
+
+            vt_main_pre_audioWavesurfer: null
         }
+    }
+})
+
+//详情组件
+const info_component = Vue.extend({
+    template: document.getElementById('vt_main_info_template').innerHTML,
+    mounted() {
+        //初始化
+    },
+    methods: {
+
     },
     data() {
         return {
@@ -149,21 +151,15 @@ const track_component = Vue.extend({
     }
 })
 
-
-//模板开发组件
-const temdev_component = Vue.extend({
-    template: `
-        <h1> tem_dev </h1>
-        `,
+//素材组件
+const mat_component = Vue.extend({
+    template: document.getElementById('vt_main_mat_template').innerHTML,
     mounted() {
-        //注册组件实例对象到store
-        this.$store.state.temdev_component = this
+        //初始化
     },
     methods: {
-        //初始化
-        init: function () {
 
-        }
+
     },
     data() {
         return {
@@ -171,33 +167,33 @@ const temdev_component = Vue.extend({
         }
     }
 })
+
 
 //设置组件
 const setting_component = Vue.extend({
-    template: document.getElementById('vt-main-setting-template').innerHTML,
+    template: document.getElementById('vt_main_setting_template').innerHTML,
     mounted() {
-        //注册组件实例对象到store
-        this.$store.state.setting_component = this
+        //初始化
+        //获取配置项
+        this.$store.state.axios_exec(
+            '/vt/getSettingProperties', {}, (res) => {
+                this.vt_main_setting_properties = res.data.data
+                //更新项目数据目录
+                this.vt_main_setting_card_projectDataPath = this.vt_main_setting_properties.projectDataPath
+            }
+        )
     },
     methods: {
-        //初始化
-        init: function () {
-            //获取配置项
-            this.$store.state.axios_exec(
-                '/vt/getSettingProperties', {}, (res) => {
-                    this.vt_main_setting_properties = res.data.data
-                    //更新项目数据目录
-                    this.vt_main_setting_card_projectDataPath = this.vt_main_setting_properties.projectDataPath
-                }
-            )
-        }
+
     },
     data() {
         return {
+            //设置面板是否展开
+            vt_header_settingDialogVisible:false,
             //项目配置项
             vt_main_setting_properties: [],
             //项目数据目录
-            vt_main_setting_card_projectDataPath: ''
+            vt_main_setting_projectDataPath: ''
 
         }
     }
@@ -208,25 +204,16 @@ const vt_component = Vue.extend({
     template: document.getElementById('vt').innerHTML,
     data() {
         return {
-            //异步等待任务数量
-            vt_async_task_num: 0,
+            
             //异步处理状态时提示信息
             vt_loadingText: '...',
-            //当前激活的功能导航栏
-            vt_main_nav_activename: 'metfile',
             //项目标题
-            vt_header_card_title: ''
+            vt_header_card_title: '-'
         }
 
     },
     methods: {
-        //初始化
-        init: function () {
-            //获取项目名
-            this.$store.state.jsx_exec('get_project_name', '', (data) => {
-                this.vt_header_card_title = data
-            })
-        }
+
     },
     computed: {
         //此刻是否为异步处理状态
@@ -238,24 +225,26 @@ const vt_component = Vue.extend({
                     return false
                 }
             }
-        }
+        },
+        ...Vuex.mapState([
+            // 异步等待任务数量
+            'vt_async_task_num'
+        ])
+        
     },
     mounted() {
-        //注册组件实例对象到store
-        this.$store.state.vt_component = this
+        //初始化
+        //获取项目名
+        this.$store.state.jsx_exec('get_project_name', '', (data) => {
+            this.vt_header_card_title = data
+        })
 
-
-        //初始化各组件数据
-        this.$store.state.metfile_component.init()
-        this.$store.state.track_component.init()
-        this.$store.state.temdev_component.init()
-        this.$store.state.setting_component.init()
-        this.$store.state.vt_component.init()
     },
     components: {
-        metfile_component,
-        track_component,
-        temdev_component,
+        matfile_component: matFile_component,
+        pre_component,
+        info_component,
+        mat_component,
         setting_component
     }
 })
--- js/src/store.js
+++ js/src/store.js
@@ -10,12 +10,13 @@ var store = new Vuex.Store({
         axiosInstance: axios.create({
             baseURL: 'http://localhost:9999'
         }),
-        //各组件实例对象
-        metfile_component: null,
-        track_component: null,
-        temdev_component: null,
-        setting_component: null,
-        vt_component: null,
+        //素材文件-表格默认选项
+        vt_main_matFile_bodyTableCurrentRow: {
+            type: '',
+            key: ''
+        },
+        //异步等待任务数量
+        vt_async_task_num: 0,
 
         //通用方法
         //弹出提示信息
@@ -27,7 +28,7 @@ var store = new Vuex.Store({
         },
         //封装adobe cep桥接方法
         jsx_exec: function (func_name, func_args, call_back) {
-            this.vt_component.vt_async_task_num += 1
+            this.vt_async_task_num += 1
             var tmp_func_args = func_name + '("' + func_args + '")'
             //预处理返回结果，如果结果异常，发送通知
             this.csInterface.evalScript(tmp_func_args, (data)=>{
@@ -36,23 +37,35 @@ var store = new Vuex.Store({
                 } else {
                     call_back(data)
                 }
-                this.vt_component.vt_async_task_num -= 1
+                this.vt_async_task_num -= 1
             
             })
         },
         //封装axios请求
         axios_exec: async function (path, body, call_back) {
-            this.vt_component.vt_async_task_num += 1
+            this.vt_async_task_num += 1
             await this.axiosInstance.post(path, body)
                 .then(call_back)
                 .catch( (error) => {
                     this.notify('error','错误',error.message)
                 })
-            this.vt_component.vt_async_task_num -= 1
+            this.vt_async_task_num -= 1
         }
 
 
     },
-    mutations: {},
+    //提交更改
+    mutations: {
+        //修改素材文件-表格默认选项
+        vt_main_matFile_bodyTableCurrentRow: function(state,val){
+            state.vt_main_matFile_bodyTableCurrentRow=val
+        },
+
+        //修改异步等待任务数量
+        vt_async_task_num: function(state,val){
+            state.vt_async_task_num=val
+        }
+    },
+    // 异步的方式提交用这个
     actions: {}
 })
