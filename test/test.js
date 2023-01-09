77ab3492752f6a0f2da0963913735f35ed95a65f
yindahu <yindahu@nibirutech.com>
2023-01-09 11:22:05 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-
 // let sleepFun = (time) => new Promise((resolve) => setTimeout(resolve, time))
 // console.log(1)
 // sleepFun(10000).then(()=>console.log('hi'))
@@ -21,4 +20,41 @@
 
 // console.log(parseFloat(Math.floor(0/100)))
 
-console.log(typeof(''))
+// var tmpList = [{'a':1.234,'b':'hi'},{'a':2.234,'b':'hi'}]
+// var ind =tmpList.indexOf({'a':1.234,'b':'hi'})
+
+// console.log(ind)
+// tmpList.splice(ind,1)
+// console.log(tmpList)
+
+
+// var tmpList = [1,2,3]
+// console.log(tmpList.indexOf(2))
+// tmpList.splice(tmpList.indexOf(2),1)
+// console.log(tmpList)
+
+// function objEquals(object1, object2) {
+//     var keys1 object.keys(object1)
+//     var keys2 Object.keys(object2)
+
+//     if (keys1.length != keys2.length) {
+//         return false
+//     }
+
+//     for (let key of keys1) {
+//         if (object1[key] != object2[key]) {
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+const util = require('util');
+
+console.log(util.isDeepStrictEqual({
+    'a': 1.234,
+    'b': 'hi'
+} , {
+    'a': 1.234,
+    'b': 'hi3'
+}))
